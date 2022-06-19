2e9ee05f32b84e4b329f3d7d4fec9ebfa289b94d
Havane <havane.palabaud@epitech.eu>
2022-06-19 20:10:10 +0200

    showcase update

---
+++ components/Showcase.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex h-full w-full">
+    <div class="flex flex-col flex-1 place-content-evenly">
+      <div
+        class="bg-tertiary h-32 w-32 flex flex-col justify-center items-center hover:scale-110"
+      >
+        <fa :icon="['fab', 'react']" class="h-3/4 text-blue-500" />
+        ReactJs
+      </div>
+      <div
+        class="bg-tertiary h-32 w-32 flex flex-col justify-center items-center hover:scale-110"
+      >
+        <fa :icon="['fab', 'vuejs']" class="h-3/4 text-green-500" />
+        VueJs
+      </div>
+      <div
+        class="bg-tertiary h-32 w-32 flex flex-col justify-center items-center hover:scale-110"
+      >
+        <fa :icon="['fab', 'vuejs']" class="h-3/4 text-green-500" />
+        VueJs
+      </div>
+      <div
+        class="bg-tertiary h-32 w-32 flex flex-col justify-center items-center hover:scale-110"
+      >
+        <fa :icon="['fab', 'vuejs']" class="h-3/4 text-green-500" />
+        VueJs
+      </div>
+    </div>
+    <div class="flex flex-col flex-1 opacity-30 bg-black"></div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
